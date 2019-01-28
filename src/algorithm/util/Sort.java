aaee4db573a857b7c3ed1a9097410d1331794886
lixilong <17610773189@163.com>
2019-01-28 17:28:02 +0800

    工具节点 补充

---
+++ src/algorithm/util/Sort.java
@@ -0,0 +1,20 @@
+package algorithm.util;
+
+/**
+ * describe:
+ *
+ * @author lixilong
+ * @date 2019/01/11
+ */
+public class Sort {
+
+
+  /*
+  链接：https://www.nowcoder.com/questionTerminal/6a296eb82cf844ca8539b57c23e6e9bf
+
+  来源：牛客网
+  八大排序：
+  基数排序 快速排序 归并排序 堆排序 冒泡排序 直接选择排序 插入排序
+  */
+
+}
---
+++ src/algorithm/util/TreeNode.java
@@ -0,0 +1,20 @@
+package algorithm.util;
+
+/**
+ * describe:
+ *
+ * @author lixilong
+ * @date 2019/01/10
+ */
+public class TreeNode {
+
+
+  //Definition for binary tree
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int x) {
+      this.val = x;
+    }
+}
